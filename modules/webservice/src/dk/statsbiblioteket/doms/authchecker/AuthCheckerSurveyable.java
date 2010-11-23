d4e04fc05b72667f94e4ad1cba57e6aec5c7def9
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2010-11-23 17:07:33 +0000

    Cutting new version

---
+++ modules/webservice/src/dk/statsbiblioteket/doms/authchecker/AuthCheckerSurveyable.java
@@ -0,0 +1,55 @@
+package dk.statsbiblioteket.doms.authchecker;
+
+import dk.statsbiblioteket.doms.domsutil.surveyable.Surveyable;
+import dk.statsbiblioteket.doms.domsutil.surveyable.Status;
+import dk.statsbiblioteket.doms.domsutil.surveyable.StatusMessage;
+import dk.statsbiblioteket.doms.domsutil.surveyable.Severity;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abr
+ * Date: Nov 23, 2010
+ * Time: 5:11:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AuthCheckerSurveyable implements Surveyable {
+    /** The name this status reports. */
+    private static final String SURVEYABLE_NAME = "AuthChecker";
+
+    /** Log for this class. */
+    private final Log log = LogFactory.getLog(getClass());
+
+    /**
+     * Always returns that status "Running".
+     * @param l Ignored
+     * @return Status "Running".
+     */
+    public Status getStatusSince(long l) {
+
+        //TODO get this to report on the static state
+        log.trace("Enter getStatusSince(" + l + ")");
+
+        Status status = new Status();
+        StatusMessage statusMessage = new StatusMessage();
+
+        statusMessage.setLogMessage(false);
+        statusMessage.setSeverity(Severity.GREEN);
+        statusMessage.setTime(System.currentTimeMillis());
+        statusMessage.setMessage("Running");
+        status.setName(SURVEYABLE_NAME);
+        status.getMessages().add(statusMessage);
+        return status;
+    }
+
+    /**
+     * Reports exactly the same as getStatusSince(0L).
+     * @return Status.
+     */
+    public Status getStatus() {
+        log.trace("Enter getStatus()");
+
+        return getStatusSince(0L);
+    }
+}
