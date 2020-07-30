cfe1498415f0a3482a2ff1f570361df0510c89ea
Kim Christensen <ktc@kb.dk>
2020-07-30 14:57:44 +0200

    fix path to configuration files for ocp deployment

--- kb-s2i/deploy.sh
+++ kb-s2i/deploy.sh
@@ -1,7 +1,7 @@
 
 
-cp -- /tmp/src/conf/ocp/logback.xml "$CONF_DIR/logback.xml"
-cp -- /tmp/src/conf/ticket-system-service.yaml "$CONF_DIR/ticket-system-service.yaml"
+cp -- /tmp/src/ticket-system-service/conf/ocp/logback.xml "$CONF_DIR/logback.xml"
+cp -- /tmp/src/ticket-system-service/conf/ticket-system-service.yaml "$CONF_DIR/ticket-system-service.yaml"
 
 sed -i 's/localhost/memcached/' "$CONF_DIR/ticket-system-service.yaml"
  
